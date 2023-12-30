e1605aefb062526d791d3f967a785e2dae1e74c8
Krzysztof Magusiak <chris.magusiak@gmail.com>
2023-12-30 14:18:12 +0100

    Disable runbot tests
    The server is not accessible for jsonrpc calls.

--- tests/runbot/conftest.py
+++ tests/runbot/conftest.py
@@ -1,51 +1,21 @@
 import logging
-import re
+import os
 
 import pytest
-import requests
 
 import odoo_connect
 
 
 @pytest.fixture(scope='session')
 def connect_params():
-    runbot_url = 'https://runbot.odoo.com/'
-    runbot_page = requests.get(runbot_url)
-    odoo_url = "http://localhost:8069/"
+    # runbot is no longer accessible for jsonrpc,
+    # try to get the server from the environment
+    odoo_url = os.environ.get("ODOO_URL", "http://localhost:8069/")
     version = "unknown"
-    # we don't use (yet) any XML parsing to keep dependencies simple
-    # find one of the following and select a URL
-    # - bundle href (row and name of the branch)
-    # - link to a sign-in action (avoid documentation and take only stable versions)
-    for m in re.finditer(
-        r'href="/runbot/(bundle)[^<]+<b>([^<]*)</b>'
-        r'|<a class="[^"]*fa-(sign-in)[^"]*" href="([^"]*)"',
-        runbot_page.text,
-    ):
-        if m.group(1) == 'bundle':
-            version = m.group(2)
-        elif (
-            re.match(r'^\d+\.\d$', version)
-            and m.group(3) == 'sign-in'
-            and 'build/html' not in m.group(4)
-        ):
-            odoo_url = m.group(4)
-            if odoo_url.startswith('/'):
-                # relative path, will redirect to server, find it
-                odoo_url = odoo_connect.odoo_rpc.urljoin(runbot_url, odoo_url)
-                resp = requests.head(odoo_url)
-                if resp.is_redirect:
-                    odoo_url = resp.next.url
-            else:
-                odoo_url = odoo_url.replace('http://', 'https://')
-            break
     logging.info("Using odoo server %s: %s", version, odoo_url)
 
     return {
         'url': odoo_url,
-        'username': 'admin',
-        'password': 'admin',
-        'check_connection': False,
     }
 
 
